a1564f366587e2270196c0e5383d363d32753c98
ubaidullahmohammed <obaidsayeed91@gmail.com>
2022-05-10 17:05:44 +0530

    10th May,2022

--- src/test/java/StringEncodingTest.java
+++ src/test/java/StringEncodingTest.java
@@ -14,7 +14,11 @@ public class StringEncodingTest {
         System.out.println(StringEncoding.solution("AABBCCDABCBDA"));
     }
     @Test
-    public void testCase_1(){
-        System.out.println(StringEncoding.solution("AAAABBBCCD"));
+    public void testCase_4(){
+        System.out.println(StringEncoding.solution("ABCDABABCCDA"));
+    }
+    @Test
+    public void testCase_5(){
+        System.out.println(StringEncoding.solution("BDACBCAEHHFD"));
     }
 }
