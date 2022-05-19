77975218c2d7ec7c62447d21855b1e0bdc7bccc1
ubaidullahmohammed <obaidsayeed91@gmail.com>
2022-05-19 18:12:53 +0530

    12th May,2022

---
+++ src/main/java/PiValue_17_05_2022.java
@@ -0,0 +1,29 @@
+//The area of a circle is defined as πr^2. Estimate π to 3 decimal places using a Monte Carlo method.
+
+import java.util.Random;
+public class PiValue_17_05_2022
+{
+    static int cir_pt = 0;
+    static int sq_pt = 0;
+    public static void solution()
+    {
+        int interval = 10000;
+        for(int i = 0;i < interval;i++)
+        {
+            Random rand = new Random();
+            double ran_x = rand.nextDouble();
+            double ran_y = rand.nextDouble();
+            double dist = ran_x*ran_x + ran_y*ran_y;
+
+            if(dist <= 1)
+            {
+                cir_pt += 1;
+            }
+            sq_pt += 1;
+
+        }
+        float pi = (float)(4*cir_pt)/sq_pt;
+        System.out.println(pi);
+
+    }
+}
---
+++ src/test/java/PiValue_17_05_2022Test.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class PiValue_17_05_2022Test {
+    @Test
+    public void testCase_1(){
+        PiValue_17_05_2022.solution();
+    }
+}
