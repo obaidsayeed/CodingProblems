631faf06fe93c2cec12b57c93eab1d8ead9735ce
ubaidullahmohammed <obaidsayeed91@gmail.com>
2022-06-09 16:11:24 +0530

    9th June,2022

--- src/test/java/CountWaysToReachBottom_09_06_2022Test.java
+++ src/test/java/CountWaysToReachBottom_09_06_2022Test.java
@@ -5,4 +5,24 @@ public class CountWaysToReachBottom_09_06_2022Test {
     public void testCase_1(){
         System.out.println(CountWaysToReachBottom_09_06_2022.solution(5,5));
     }
+
+    @Test
+    public void testCase_2(){
+        System.out.println(CountWaysToReachBottom_09_06_2022.solution(4,3));
+    }
+
+    @Test
+    public void testCase_3(){
+        System.out.println(CountWaysToReachBottom_09_06_2022.solution(1,4));
+    }
+
+    @Test
+    public void testCase_4(){
+        System.out.println(CountWaysToReachBottom_09_06_2022.solution(1,1));
+    }
+
+    @Test
+    public void testCase_5(){
+        System.out.println(CountWaysToReachBottom_09_06_2022.solution(6,1));
+    }
 }
