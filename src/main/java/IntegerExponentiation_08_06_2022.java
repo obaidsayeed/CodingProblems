fc0098876b92c5f92dac489d5838680456949306
ubaidullahmohammed <obaidsayeed91@gmail.com>
2022-06-09 16:05:08 +0530

    9th June,2022

--- src/main/java/IntegerExponentiation_08_06_2022.java
+++ src/main/java/IntegerExponentiation_08_06_2022.java
@@ -7,13 +7,16 @@
 //For example, pow(2, 10) should return 1024.
 public class IntegerExponentiation_08_06_2022 {
     public static int solution(int m, int n) {
-        if (n == 1){
-            return m;
+        if(n < 0){
+            return 1/solution(m,n*-1);
         }
-        if (n % 2 != 0) {
-            return m * solution(m, (n - 1)/2);
+        if(n == 0){
+            return 1;
         }
-        return solution(m, n / 2) * solution(m, n / 2);
+        if(n % 2 != 0) {
+            return m * solution(m, (n - 1)/2) * solution(m,(n - 1)/2);
+        }
 
+        return solution(m, n / 2) * solution(m, n / 2);
     }
 }
--- src/test/java/IntegerExponentiation_08_06_2022Test.java
+++ src/test/java/IntegerExponentiation_08_06_2022Test.java
@@ -5,4 +5,20 @@ public class IntegerExponentiation_08_06_2022Test {
     public void testCase_1(){
         System.out.println(IntegerExponentiation_08_06_2022.solution(2,16));
     }
+    @Test
+    public void testCase_2(){
+        System.out.println(IntegerExponentiation_08_06_2022.solution(3,5));
+    }
+    @Test
+    public void testCase_3(){
+        System.out.println(IntegerExponentiation_08_06_2022.solution(2,0));
+    }
+    @Test
+    public void testCase_4(){
+        System.out.println(IntegerExponentiation_08_06_2022.solution(2,7));
+    }
+    @Test
+    public void testCase_5(){
+        System.out.println(IntegerExponentiation_08_06_2022.solution(2,-1));
+    }
 }
