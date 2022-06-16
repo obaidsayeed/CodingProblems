3f3ec1cc03206905681bd349f8807054bf9dff02
ubaidullahmohammed <obaidsayeed91@gmail.com>
2022-06-16 18:41:08 +0530

    Coding Problem

---
+++ src/test/java/NthPerfectNumber_16_06_2022Test.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+public class NthPerfectNumber_16_06_2022Test {
+
+    @Test
+    public void testCase_1(){
+        //System.out.println(NthPerfectNumber_16_06_2022.solution1(11));
+        System.out.println(NthPerfectNumber_16_06_2022.solution2(11));
+    }
+
+    @Test
+    public void testCase_2(){
+        //System.out.println(NthPerfectNumber_16_06_2022.solution1(5));
+        System.out.println(NthPerfectNumber_16_06_2022.solution2(5));
+    }
+
+    @Test
+    public void testCase_3(){
+        //System.out.println(NthPerfectNumber_16_06_2022.solution1(2));
+        System.out.println(NthPerfectNumber_16_06_2022.solution2(2));
+    }
+}
