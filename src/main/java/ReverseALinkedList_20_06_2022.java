301bde891fd3bd524b4cc6e7668a37c4d9fcecac
ubaidullahmohammed <obaidsayeed91@gmail.com>
2022-06-20 12:06:51 +0530

    Coding Problem

---
+++ src/main/java/ReverseALinkedList_20_06_2022.java
@@ -0,0 +1,41 @@
+//Asked by Google.
+//
+//Given the head of a singly linked list, reverse it in-place.
+//Constraints:
+//
+//The number of nodes in the list is the range [0, 5000].
+//-5000 <= Node.val <= 5000
+
+class Node{
+    int val;
+    Node next;
+    Node(int val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class ReverseALinkedList_20_06_2022 {
+
+    Node head;
+    ReverseALinkedList_20_06_2022(){
+        head = null;
+    }
+
+    public static Node solution(Node head){
+        Node prev = null;
+        Node curr = head;
+        Node temp = null;
+
+        while(curr != null){
+            temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        head = prev;
+        return prev;
+    }
+
+}
---
+++ src/test/java/ReverseALinkedList_20_06_2022Test.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+public class ReverseALinkedList_20_06_2022Test {
+
+    @Test
+    public void testCase_1(){
+        ReverseALinkedList_20_06_2022 list = new ReverseALinkedList_20_06_2022();
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        Node temp = head;
+        while (temp != null){
+            System.out.print(temp.val + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+
+        head = list.solution(head);
+        temp = head;
+        while (temp != null){
+            System.out.print(temp.val + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+
+    }
+}
