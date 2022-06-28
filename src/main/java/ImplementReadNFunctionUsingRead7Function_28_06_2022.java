d469d82b4ddff8192730c6398be9a6971a46d917
ubaidullahmohammed <obaidsayeed91@gmail.com>
2022-06-28 11:52:51 +0530

    Coding Problem

---
+++ src/main/java/ImplementReadNFunctionUsingRead7Function_28_06_2022.java
@@ -0,0 +1,30 @@
+import jdk.nashorn.internal.runtime.regexp.joni.ast.StringNode;
+
+public class ImplementReadNFunctionUsingRead7Function_28_06_2022 {
+    String file = "";
+    int offset = 0;
+    String buffer = "";
+    public ImplementReadNFunctionUsingRead7Function_28_06_2022(String file){
+        this.file = file;
+        this.offset = 0;
+        this.buffer = "";
+    }
+
+    public String read7(){
+        int start = this.offset;
+        int end = Math.min(this.offset + 7,this.file.length());
+        this.offset = end;
+        return this.file.substring(start,end);
+    }
+
+    public String readN(int n){
+        while (this.buffer.length() < n){
+            String extra = this.read7();
+            if (extra.length() == 0)break;
+            this.buffer += extra;
+        }
+        String resultString = this.buffer.substring(0,n);
+        this.buffer = this.buffer.substring(n);
+        return resultString;
+    }
+}
---
+++ src/test/java/ImplementReadNFunctionUsingRead7Function_28_06_2022Test.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class ImplementReadNFunctionUsingRead7Function_28_06_2022Test {
+    @Test
+    public void testCase_1(){
+        ImplementReadNFunctionUsingRead7Function_28_06_2022 obj = new ImplementReadNFunctionUsingRead7Function_28_06_2022("Hello World");
+        System.out.println(obj.readN(10));
+    }
+}
