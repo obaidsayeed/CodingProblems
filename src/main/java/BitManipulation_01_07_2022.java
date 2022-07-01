d331f0bb34c43000dadc71102a440fc4a565a4e6
ubaidullahmohammed <obaidsayeed91@gmail.com>
2022-07-01 11:53:51 +0530

    Coding Problem

---
+++ src/main/java/BitManipulation_01_07_2022.java
@@ -0,0 +1,19 @@
+//Asked by Facebook.
+//
+//Given three 32-bit integers x, y, and b, return x if b is 1 and y if b is 0,
+// using only mathematical or bit operations. You can assume b can only be 1 or 0.
+
+public class BitManipulation_01_07_2022 {
+
+    public static int solution(int x,int y, int b){
+        int result = x*b + y*(1-b);
+        return result;
+    }
+
+    public static int solution2(int x, int y, int b){
+        if ((b&1) == 1){
+            return x;
+        }
+        return y;
+    }
+}
