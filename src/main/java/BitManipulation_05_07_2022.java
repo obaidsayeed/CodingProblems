6cf2a4b591717067f8006f211ba63a8db68eb090
ubaidullahmohammed <obaidsayeed91@gmail.com>
2022-07-05 18:54:47 +0530

    Coding Problem

--- src/main/java/BitManipulation_05_07_2022.java
+++ src/main/java/BitManipulation_05_07_2022.java
@@ -3,7 +3,7 @@
 //Given three 32-bit integers x, y, and b, return x if b is 1 and y if b is 0,
 // using only mathematical or bit operations. You can assume b can only be 1 or 0.
 
-public class BitManipulation_01_07_2022 {
+public class BitManipulation_05_07_2022 {
 
     public static int solution(int x,int y, int b){
         int result = x*b + y*(1-b);
---
+++ src/test/java/BitManipulation_05_07_2022Test.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class BitManipulation_05_07_2022Test {
+
+    @Test
+    public void testCase_1(){
+        System.out.println(BitManipulation_05_07_2022.solution(90,78,1));
+        System.out.println(BitManipulation_05_07_2022.solution2(82,66,0));
+    }
+}
